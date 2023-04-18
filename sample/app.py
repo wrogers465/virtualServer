be0bdd36d6d321053a60b4380590ff7aadeb439f
Will Rogers <wrogers465@gmail.com>
2023-04-18 14:54:16 -0400

    Removed json run time logic. Added send-ok function to test network connection

--- sample/app.py
+++ sample/app.py
@@ -1,20 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, make_response
 from lib import *
 
-
 app = Flask(__name__)
+
 
 @app.route('/turn-on-desktop')
 def turn_on_desktop():
     send_magic_packet('F0-2F-74-18-8E-56')
-    store_run_time()
 
-@app.route('/get-last-run-time', methods=['GET'])
-def get_last_run_time():
-    data = get_run_time()
 
-    return jsonify(data)
-
+@app.route('/send-ok')
+def send_ok():
+    response = make_response("Success", 200)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- sample/lib.py
+++ sample/lib.py
@@ -1,6 +1,4 @@
-import json
 import socket
-import time
 
 
 def send_magic_packet(mac_address, broadcast_address='255.255.255.255', port=9):
@@ -16,17 +14,6 @@ def send_magic_packet(mac_address, broadcast_address='255.255.255.255', port=9):
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         # Send the magic packet to the broadcast address using the specified port (default is 9)
         sock.sendto(magic_packet, (broadcast_address, port))
-
-def store_run_time():
-    data = {'last_run_time': time.time()}
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
-
-def get_run_time():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-
-    return data
 
 
 if __name__ == '__main__':
