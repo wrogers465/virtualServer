7aa77ad18491d74f7566fabbbceb4f9a4339894a
Will Rogers <wrogers465@gmail.com>
2023-04-18 15:41:09 -0400

    Functionality now turns on computer if off. Then starts stream after connection confirmed

--- sample/app.py
+++ sample/app.py
@@ -1,18 +1,37 @@
+import requests
+import time
 from flask import Flask, make_response
 from lib import *
 
 app = Flask(__name__)
-
-
-@app.route('/turn-on-desktop')
-def turn_on_desktop():
-    send_magic_packet('F0-2F-74-18-8E-56')
+run_on_connect = None
 
 
 @app.route('/send-ok')
 def send_ok():
+    global run_on_connect
+    attempts = 90
+    while attempts > 0:
+        try:
+            if run_on_connect:
+                run_on_connect()
+                run_on_connect = None
+        except requests.exceptions.ConnectionError:
+            attempts -= 1
+            time.sleep(0.5)
+
     response = make_response("Success", 200)
     return response
+
+@app.route('/start-lightning-stream')
+def start_lightning_stream():
+    global run_on_connect
+    start_stream = lambda: requests.get('http://192.168.0.13:5000/start-lightning-stream')
+    try:
+        start_stream()
+    except requests.exceptions.ConnectionError:
+        send_magic_packet('F0-2F-74-18-8E-56')
+        run_on_connect = start_stream
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
