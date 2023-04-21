691c7c0c238532ea561fff45f0a5e04ebf8d8667
Will Rogers <wrogers465@gmail.com>
2023-04-21 02:45:55 -0400

    Updated message again

--- sample/app.py
+++ sample/app.py
@@ -28,7 +28,7 @@ def send_ok():
                 time.sleep(0.5)
 
     time.sleep(0.5)
-    res = make_response("Success2", 200)
+    res = make_response("Success3", 200)
     return res        
 
 
